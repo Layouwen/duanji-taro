5d3256e65fadee3b3298a44ddc61e95b7d42a019
liangyouwen <layouwen@gmail.com>
2020-09-19 18:23:07 +0800

    原生

--- src/pages/test/test.jsx
+++ src/pages/test/test.jsx
@@ -1,42 +1,73 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 import Taro from "@tarojs/taro"
-import { Button, View } from "@tarojs/components"
+import { Button, Canvas, View } from "@tarojs/components"
 import { styled } from "linaria/react"
-import Card from "../../canvas/card"
+
+const WIDTH = 660
+const HEIGHT = 960
 
 const Container = styled(View)`
   height: 100vh;
+  > Canvas {
+    border: 1px solid red;
+  }
 `
 
-let imagePath = ""
-
 export default () => {
-  const [template, setTemplate] = useState()
-
-  useEffect(() => {
-    const x = new Card().palette()
-    setTemplate(x)
-  }, [])
-
-  const onImgOK = (e) => {
-    console.log(222, e)
+  const draw = () => {
+    const ctx = Taro.createCanvasContext("share")
+    console.log(ctx)
+    drawBg(ctx)
+    drawHeader(ctx)
   }
 
-  const onImgErr = (e) => {
-    console.log(111, e)
+  const drawBg = (ctx) => {
+    ctx.rect(0, 0, WIDTH, HEIGHT)
+    ctx.setStrokeStyle("#ffc7c7")
+    ctx.stroke()
+
+    const grd = ctx.createLinearGradient(0, 0, 0, WIDTH)
+    grd.addColorStop(0, "#ffdede")
+    grd.addColorStop(1, "#000")
+
+    ctx.setFillStyle(grd)
+    ctx.fillRect(0, 0, WIDTH, HEIGHT)
   }
 
-//  const saveImage = () => {
-//    console.log(imagePath)
-//    Taro.saveImageToPhotosAlbum({
-//      filePath: imagePath,
-//    })
-//  }
+  const drawHeader = (ctx) => {
+    ctx.moveTo(107, 42)
+    ctx.lineTo(107 + 139, 42)
 
+    ctx.moveTo(414, 42)
+    ctx.lineTo(414 + 139, 42)
+
+    ctx.setStrokeStyle("#fe8787")
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.arc(107 + 139 + 5, 42, 5, 0, 2 * Math.PI)
+    ctx.fillStyle = "#fe8787"
+    ctx.fill()
+
+    ctx.beginPath()
+    ctx.arc(414 + 5, 42, 5, 0, 2 * Math.PI)
+    ctx.fillStyle = "#fe8787"
+    ctx.fill()
+
+    ctx.font = "32px STSongti-SC-Regular,sans-serif"
+    ctx.fillStyle = "#fe8787"
+    ctx.setTextAlign("center")
+    ctx.setTextBaseline("middle")
+    ctx.fillText("邀请卡", WIDTH / 2, 42)
+  }
   return (
     <Container>
-      <painter customStyle='margin-left:40rpx' palette={template} imgOK={e => onImgOK(e)}/>
-      {/*<Button onClick={saveImage}>11</Button>*/}
+      <Canvas
+        calssName='canvas'
+        canvasId='share'
+        style={`width: ${WIDTH}px; height: ${HEIGHT}px;`}
+      />
+      <Button onClick={draw}> adfasdf 1 </Button>
     </Container>
   )
 }
