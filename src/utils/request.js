5a1dddff472731570915b229a9eebdf071da6114
laoxiangru <laoxiangru.com>
2020-09-22 16:45:13 +0800

    修改baseURL

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,11 @@
-import Taro from "@tarojs/taro"
-import {setCookies, getCookies} from "./cookie"
-import {login} from "./auth"
+import Taro from "@tarojs/taro";
+import { setCookies, getCookies } from "./cookie";
+import { login } from "./auth";
 
 const defaultOptions = {
-  baseURL: "http://192.168.31.186:8003",
-}
+  baseURL: "https://duanji.gitmen.cn",
+  // baseURL: "http://127.0.0.1:8003",
+};
 
 function handHeader(header) {
   /**
@@ -16,74 +17,80 @@ function handHeader(header) {
 
   const defaultHeader = {
     "Content-Type": "application/json",
-  }
-  let nextHeader = {...header, ...defaultHeader}
-  const cookies = getCookies()
+  };
+  let nextHeader = { ...header, ...defaultHeader };
+  const cookies = getCookies();
 
   if (cookies) {
-    nextHeader["Cookie"] = cookies
+    nextHeader["Cookie"] = cookies;
   }
 
-  return nextHeader
+  return nextHeader;
 }
 
 async function request(
   method,
   path,
-  {data = {}, header = {}} = {},
-  {baseURL}
+  { data = {}, header = {} } = {},
+  { baseURL }
 ) {
-  let response = {}
-  Taro.showNavigationBarLoading()
+  let response = {};
+  Taro.showNavigationBarLoading();
   try {
+    console.log({
+      method,
+      data,
+      header: handHeader(header),
+      url: baseURL + (path.startsWith("/") ? path : "/" + path),
+    });
+
     response = await Taro.request({
       method,
       data,
       header: handHeader(header),
       url: baseURL + (path.startsWith("/") ? path : "/" + path),
-    })
-
+    });
   } catch (reason) {
-    return Promise.reject(reason)
+    return Promise.reject(reason);
   } finally {
-    Taro.hideNavigationBarLoading()
+    Taro.hideNavigationBarLoading();
   }
 
-  const {data: responseData, header: responseHeader} = response
+  const { data: responseData, header: responseHeader } = response;
 
   // 设置 cookie 到 localstorage 中
-  setCookies(responseHeader)
+  setCookies(responseHeader);
 
-  const error_code = parseInt(responseData.error_code, 10)
+  const error_code = parseInt(responseData.error_code, 10);
 
   if (error_code === 403) {
-    await login()
-    return await request(...arguments)
+    await login();
+    return await request(...arguments);
   }
   if (error_code !== 0) {
-    await Taro.showToast({title: responseData.error_message, icon: "none"})
-    return Promise.reject(responseData)
+    await Taro.showToast({ title: responseData.error_message, icon: "none" });
+    return Promise.reject(responseData);
   }
 
-  return responseData.result
+  return responseData.result;
 }
 
 class Request {
   constructor(options = {}) {
-    this.options = {...defaultOptions, ...options}
+    this.options = { ...defaultOptions, ...options };
   }
 
   config(options) {
-    return new Request(options)
+    return new Request(options);
   }
 
-  request = (...args) => request(...args, this.options)
+  request = (...args) => request(...args, this.options);
 
-  get = (path, query = {}) => this.request("GET", path, {data: query})
-  post = (path, body = {}) => this.request("POST", path, {data: body})
-  patch = (path, body = {}) => this.request("PATCH", path, {data: body})
-  put = (path, body = {}) => this.request("PUT", path, {data: body})
-  delete = (path) => this.request("DELETE", path, {})
+  get = (path, query = {}) => this.request("GET", path, { data: query });
+  post = (path, body = {}) => this.request("POST", path, { data: body });
+  patch = (path, body = {}) => this.request("PATCH", path, { data: body });
+  put = (path, body = {}) => this.request("PUT", path, { data: body });
+  delete = (path) => this.request("DELETE", path, {});
 }
 
-export default new Request()
+export default new Request();
